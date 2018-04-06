40b5795c25f9d5db53d4ed4f3e9e70ccb7528a60
Pim Otte <potte@quintor.nl>
2018-04-06 16:23:48 +0200

    Undo nonsense

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import SecretPusher from './components/ProfileCreator';
 
 import {Rspan} from 'oo7-react';
 /* import {bonds, options as bonds_options} from 'oo7-parity';*/
@@ -44,7 +43,6 @@ class App extends Component {
     return (
       <div className="App">
           {this.state.isLoggedIn ? <Dashboard handleSignOut={this.handleSignOut.bind(this)}/> : <Login handleSignIn={this.handleSignIn.bind(this)}/>}
-          <SecretPusher></SecretPusher>
       </div>
     );
   }
