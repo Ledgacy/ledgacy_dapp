e74c4a13b135dec9512cd2f0814207d5cb5f9867
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-04-06 19:59:27 +0200

    page switching

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react'
 import { Tab } from 'semantic-ui-react'
-import { Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react';
+import { Container, Sidebar, Segment, Button, Menu, Image, Icon, Header } from 'semantic-ui-react';
 import {SecretsList} from './SecretsList.js'
 
+/* import SecretsPage from './SecretsPage.js';
+ * import TrusteesPage from './TrusteesPage.js';
+ * import NotificationsPage from './NotificationsPage.js';*/
 
 import logo from '../ledgacy_logo.svg';
 
@@ -14,22 +17,45 @@ currentPage: 'secrets'
         }
     }
 
+    changePage = (page) => {
+        this.setState({...this.state, currentPage: page})
+    }
+
     render = () => {
+
+        let page;
+            if(this.state.currentPage == 'secrets'){
+                page = (
+                    <Container fluid>
+                    <Header as='header'>Secrets</Header>
+                    <SecretsList />
+                    </Container>
+                )
+                /* page = <SecretsPage />;*/
+            }else if(this.state.currentPage === 'trustees'){
+                page = <div>This page is still empty.</div>
+                /* page = <TrusteesPage />;*/
+            }else{
+                page = <div>This page is still empty.</div>
+                /* page = <NotificationsPage />;*/
+            }
+
+
         return (
                 <Sidebar.Pushable as={Segment}>
                     <Sidebar as={Menu} animation='push' width='thin' visible={true} icon='labeled' vertical inverted pointing>
                         <Menu.Item name='home'>
                             <img src={logo} className="App-menu-logo" alt="logo" />
                         </Menu.Item>
-                        <Menu.Item name='secrets' active={this.state.currentPage === 'secrets'}>
+                        <Menu.Item name='secrets' active={this.state.currentPage === 'secrets'} onClick={this.changePage}>
                             <Icon name='unlock alternate' />
                             Secrets
                         </Menu.Item>
-                        <Menu.Item name='trustees' active={this.state.currentPage === 'trustees'}>
+                        <Menu.Item name='trustees' active={this.state.currentPage === 'trustees'} onClick={this.changePage}>
                             <Icon name='protect' />
                             Trustees
                         </Menu.Item>
-                        <Menu.Item name='notifications' active={this.state.currentPage === 'notifications'}>
+                        <Menu.Item name='notifications' active={this.state.currentPage === 'notifications'} onClick={this.changePage}>
                             <Icon name='mail outline' />
                             Notifications
                         </Menu.Item>
@@ -40,14 +66,10 @@ currentPage: 'secrets'
                     </Sidebar>
                     <Sidebar.Pusher>
                         <Segment basic>
-                            <Header as='header'>
-
-                            </Header>
-                            <SecretsList />
+                            {page}
                         </Segment>
                     </Sidebar.Pusher>
                 </Sidebar.Pushable>
-
         );
     }
 }
